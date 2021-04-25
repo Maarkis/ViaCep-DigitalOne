35a4ada8b201e13d158c671a29e98fe4a3ca9238
Jean Markis <jeanmarkis85@gmail.com>
2021-04-25 19:09:56 -0300

    implemented table Material Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+import { TableMaterialAngularComponent } from './components/table-material-angular/table-material-angular.component';
+import {MatTableModule} from '@angular/material/table';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableMaterialAngularComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatTableModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
